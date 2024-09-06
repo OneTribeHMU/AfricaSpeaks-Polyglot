a452dee319286b1eef043b995699a64b04dfe0a6
OneTribeHMU <140870774+OneTribeHMU@users.noreply.github.com>
2024-09-06 08:39:52 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'natural-amber': '#FCD34D',
+        'deep-amber': '#B45309',
+        'sunset-orange': '#F97316',
+      },
+    },
+  },
+  plugins: [],
+}
